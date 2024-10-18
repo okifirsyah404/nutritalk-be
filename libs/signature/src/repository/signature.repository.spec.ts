3e7648302ac81d8079671798f3c3539692a6a97e
Oki Firdaus Syah Putra <85375019+okifirsyah404@users.noreply.github.com>
2024-10-18 09:41:38 +0700

    Update signature.repository.spec.ts

--- libs/signature/src/repository/signature.repository.spec.ts
+++ libs/signature/src/repository/signature.repository.spec.ts
@@ -1,13 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/require-await */
 import { TestBed } from '@automock/jest';
-import { PrismaService } from '@database/prisma';
+import { ISignature, PrismaService } from '@database/prisma';
 import { SignatureRepository } from './signature.repository';
 
 describe('SignatureRepository Unit Test', () => {
   let repository: SignatureRepository;
   let prisma: jest.Mocked<PrismaService>;
 
+  const mockData: ISignature = {
+    id: '1',
+    signature: 'secretSignature',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
   beforeEach(() => {
-    const { unit, unitRef } = TestBed.create(SignatureRepository).compile();
+    const { unit, unitRef } = TestBed.create(SignatureRepository)
+      .mock(PrismaService)
+      .using({
+        signature: {
+          findUnique: async (signature: string) => {
+            return signature === mockData.signature ? mockData : null;
+          },
+          create: async (signature: string) => mockData,
+          delete: async (signature: string) => mockData,
+        },
+      })
+      .compile();
 
     repository = unit;
     prisma = unitRef.get(PrismaService);
