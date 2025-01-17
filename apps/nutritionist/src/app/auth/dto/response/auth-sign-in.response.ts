292936bcc5b070d1e5dbbb9d5ea5857290370cc4
Oki Firdaus Syah Putra <85375019+okifirsyah404@users.noreply.github.com>
2025-01-18 04:06:37 +0700

    Fix: add role field on auth

--- apps/nutritionist/src/app/auth/dto/response/auth-sign-in.response.ts
+++ apps/nutritionist/src/app/auth/dto/response/auth-sign-in.response.ts
@@ -1,15 +1,24 @@
 import { IJwtToken } from "@jwt/app-jwt";
+import { AccountRole } from "@prisma/client";
 
 export class AuthSignInResponse implements IJwtToken {
-	private constructor({ accessToken, refreshToken }: IJwtToken) {
+	private constructor({
+		accessToken,
+		refreshToken,
+		accountRole,
+	}: IJwtToken & { accountRole: AccountRole }) {
 		this.accessToken = accessToken;
 		this.refreshToken = refreshToken;
+		this.accountRole = accountRole;
 	}
 
 	readonly accessToken: string;
 	readonly refreshToken: string;
+	readonly accountRole: AccountRole;
 
-	static fromEntity(entity: IJwtToken): AuthSignInResponse {
+	static fromEntity(
+		entity: IJwtToken & { accountRole: AccountRole },
+	): AuthSignInResponse {
 		return new AuthSignInResponse(entity);
 	}
 
@@ -18,5 +27,6 @@ export class AuthSignInResponse implements IJwtToken {
 			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ",
 		refreshToken:
 			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ",
+		accountRole: AccountRole.NUTRITIONIST,
 	});
 }
