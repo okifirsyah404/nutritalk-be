826389f82a2697b6974727107482e41bd1875f34
Oki Firdaus Syah Putra <okifirsyah404@gmail.com>
2025-01-25 15:06:29 +0700

    Feat: change price validation

--- apps/nutritionist/src/app/price/dto/request/update-price.request.ts
+++ apps/nutritionist/src/app/price/dto/request/update-price.request.ts
@@ -1,20 +1,39 @@
+import { PriceValidationMessage } from "@constant/message";
 import { ApiProperty } from "@nestjs/swagger";
-import { IsInt, IsNotEmpty, Min } from "class-validator";
+import { IsInt, IsNotEmpty, Max, Min } from "class-validator";
 
 export class UpdatePriceRequest {
 	@ApiProperty({
 		example: 50_000,
 	})
-	@Min(50_000)
-	@IsInt({})
-	@IsNotEmpty()
+	@Min(50_000, {
+		message: PriceValidationMessage.ERR_ONLINE_PRICE_MIN,
+	})
+	@Max(10_000_000, {
+		message: PriceValidationMessage.ERR_ONLINE_PRICE_MAX,
+	})
+	@IsInt({
+		message: PriceValidationMessage.ERR_ONLINE_PRICE_MUST_BE_NUMBER,
+	})
+	@IsNotEmpty({
+		message: PriceValidationMessage.ERR_ONLINE_PRICE_REQUIRED,
+	})
 	online: number;
 
 	@ApiProperty({
 		example: 50_000,
 	})
-	@Min(50_000)
-	@IsInt({})
-	@IsNotEmpty()
+	@Min(50_000, {
+		message: PriceValidationMessage.ERR_OFFLINE_PRICE_MIN,
+	})
+	@Max(10_000_000, {
+		message: PriceValidationMessage.ERR_OFFLINE_PRICE_MAX,
+	})
+	@IsInt({
+		message: PriceValidationMessage.ERR_OFFLINE_PRICE_MUST_BE_NUMBER,
+	})
+	@IsNotEmpty({
+		message: PriceValidationMessage.ERR_OFFLINE_PRICE_REQUIRED,
+	})
 	offline: number;
 }
