0e7716d85b2041f533eba3a85f0d17d898cda580
Oki Firdaus Syah Putra <okifirsyah404@gmail.com>
2025-02-04 06:31:07 +0700

    Feat: revamp nutritionist  price

--- apps/main/src/app/nutritionist/price/nutritionist-price.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@nestjs/common";
-import { NutritionistPriceService } from "./nutritionist-price.service";
-
-@Controller("nutritionist-price")
-export class NutritionistPriceController {
-	constructor(
-		private readonly nutritionistPriceService: NutritionistPriceService,
-	) {}
-}
