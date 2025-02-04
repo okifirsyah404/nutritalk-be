f0ba1216ea97673d4076311c5dcf12d7725c795f
Oki Firdaus Syah Putra <okifirsyah404@gmail.com>
2025-02-04 10:23:05 +0700

    Feat: revamp nutritionist profile

--- apps/main/src/app/nutritionist/nutritionist.app.module.ts
+++ apps/main/src/app/nutritionist/nutritionist.app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from "@nestjs/common";
 import { NutritionistAccountModule } from "./account/nutritionist-account.module";
 import { NutritionistAuthModule } from "./auth/nutritionist-auth.module";
-import { NutritionistPriceModule } from "./price/nutritionist-price.module";
 import { NutritionistCertificateModule } from "./certificate/nutritionist-certificate.module";
 import { NutritionistOccupationModule } from "./occupation/nutritionist-occupation.module";
+import { NutritionistPriceModule } from "./price/nutritionist-price.module";
+import { NutritionistProfileModule } from "./profile/nutritionist-profile.module";
 
 @Module({
 	imports: [
 		NutritionistAuthModule,
 		NutritionistAccountModule,
+		NutritionistProfileModule,
 		NutritionistPriceModule,
 		NutritionistCertificateModule,
 		NutritionistOccupationModule,
---
+++ apps/main/src/app/nutritionist/profile/dto/request/nutritionist-update-profile.request.ts
@@ -0,0 +1,75 @@
+import { nullToUndefined } from "@common";
+import {
+	AddressValidationMessage,
+	DateOfBirthValidationMessage,
+	NameValidationMessage,
+	PhoneNumberValidationMessage,
+	PlaceOfBirthValidationMessage,
+} from "@constant/message";
+import { IProfileEntity } from "@contract";
+import { ApiProperty } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
+import { IsDate, IsOptional, IsPhoneNumber, IsString } from "class-validator";
+
+export class NutritionistUpdateProfileRequest
+	implements
+		Partial<
+			Pick<
+				IProfileEntity,
+				"name" | "phoneNumber" | "address" | "placeOfBirth" | "dateOfBirth"
+			>
+		>
+{
+	@ApiProperty({
+		example: "John Doe",
+	})
+	@Transform(nullToUndefined)
+	@IsString({
+		message: NameValidationMessage.NAME_MUST_BE_STRING,
+	})
+	@IsOptional()
+	readonly name?: string;
+
+	@ApiProperty({
+		example: "081234567890",
+	})
+	@Transform(nullToUndefined)
+	@IsPhoneNumber("ID", {
+		message: PhoneNumberValidationMessage.PHONE_NUMBER_INVALID,
+	})
+	@IsString({
+		message: PhoneNumberValidationMessage.PHONE_NUMBER_MUST_BE_STRING,
+	})
+	@IsOptional()
+	readonly phoneNumber?: string;
+
+	@ApiProperty({
+		example: "Jl. Mastrip",
+	})
+	@Transform(nullToUndefined)
+	@IsString({
+		message: AddressValidationMessage.ADDRESS_MUST_BE_STRING,
+	})
+	@IsOptional()
+	readonly address?: string;
+
+	@ApiProperty({
+		example: "Jember",
+	})
+	@Transform(nullToUndefined)
+	@IsString({
+		message: PlaceOfBirthValidationMessage.PLACE_OF_BIRTH_MUST_BE_STRING,
+	})
+	@IsOptional()
+	readonly placeOfBirth?: string;
+
+	@ApiProperty({
+		example: new Date(),
+	})
+	@Transform(({ value }) => (value ? new Date(value) : undefined))
+	@IsDate({
+		message: DateOfBirthValidationMessage.DATE_OF_BIRTH_MUST_BE_DATE,
+	})
+	@IsOptional()
+	readonly dateOfBirth?: Date;
+}
