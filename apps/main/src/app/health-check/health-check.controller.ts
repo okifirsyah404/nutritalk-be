8a5c13699ca6ed8a79007ebf3aad81d279885248
Oki Firdaus Syah Putra <85375019+okifirsyah404@users.noreply.github.com>
2025-02-05 13:18:46 +0700

    Feat: health check

--- apps/main/src/app/health-check/health-check.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from "@nestjs/common";
-import { HealthCheckService } from "./health-check.service";
-
-@Controller("health-check")
-export class HealthCheckController {
-	constructor(private readonly healthCheckService: HealthCheckService) {}
-}
