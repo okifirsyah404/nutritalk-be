c7abbb68631bc2faacaa7168da0ca65cd71b57bb
Oki Firdaus Syah Putra <85375019+okifirsyah404@users.noreply.github.com>
2025-02-06 21:40:38 +0700

    Fix: lower bcrypt round

--- prisma/seed/helper/crypto-helper.ts
+++ prisma/seed/helper/crypto-helper.ts
@@ -1,14 +1,14 @@
-import crypto from 'crypto';
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 export function generateRandomString(length: number): string {
-  const buffer = crypto.randomBytes(length / 2);
+	const buffer = crypto.randomBytes(length / 2);
 
-  return buffer.toString('hex');
+	return buffer.toString("hex");
 }
 
 export async function hashPassword(password: string): Promise<string> {
-  const saltRounds: number = parseInt(process.env.SALT_ROUNDS || '10');
-  const salt = await bcrypt.genSalt(saltRounds);
-  return await bcrypt.hash(password, salt);
+	const saltRounds: number = parseInt(process.env.SALT_ROUNDS || "8");
+	const salt = await bcrypt.genSalt(saltRounds);
+	return await bcrypt.hash(password, salt);
 }
